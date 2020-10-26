6cf8fa673a08ef64b5cfebb7fbd7b8a78f47bac2
Muhsar <jewbreel1@gmail.com>
2020-10-26 07:29:58 +0100

    SMS v2 first push

---
+++ client/src/servers.js
@@ -0,0 +1,5 @@
+export const OwnerServer = 'http://localhost:5000/api/owner'
+export const TeacherServer = 'http://localhost:5000/api/teacher'
+export const ParentServer = 'http://localhost:5000/api/parent'
+
+
